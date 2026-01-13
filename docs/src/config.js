f9e15225d9bc256543166beaf1ff5c98731be383
Ahmediiibrahem <ahmediiibrahem72@gmail.com>
2026-01-13 09:10:38 +0300

    Create config.js

---
+++ docs/src/config.js
@@ -0,0 +1,7 @@
+// docs/src/config.js
+export const DATA_SOURCE = {
+  // ✅ حط هنا رابط الـ CSV بتاع Google Sheet بعد Publish
+  // مثال شكله بيكون:
+  // https://docs.google.com/spreadsheets/d/e/XXXX/pub?output=csv
+  cashCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRS7cczvXb0WR0xiBu_szXcQ8faugzm0YjKrzcGBChdumMF9TjyK8rUnwJIzqWSoGKC2Yo713s4nxqm/pub?gid=0&single=true&output=csv"
+};
