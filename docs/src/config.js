c2ed61fa36c996598e8094672f177246ec080051
Ahmediiibrahem <ahmediiibrahem72@gmail.com>
2026-01-13 09:40:44 +0300

    Update config.js

--- docs/src/config.js
+++ docs/src/config.js
@@ -1,5 +1,6 @@
 // docs/src/config.js
 export const DATA_SOURCE = {
+  // Google Sheets (Published CSV)
   cashCsvUrl:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRS7cczvXb0WR0xiBu_szXcQ8faugzm0YjKrzcGBChdumMF9TjyK8rUnwJIzqWSoGKC2Yo713s4nxqm/pub?gid=0&single=true&output=csv"
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRS7cczvXb0WR0xiBu_szXcQ8faugzm0YjKrzcGBChdumMF9TjyK8rUnwJIzqWSoGKC2Yo713s4nxqm/pub?gid=0&single=true&output=csv",
 };
